28b24886dd0f3fd56482b30b8b0841211e3efec4
Tej271 <utu8811@gmail.com>
2025-01-12 09:31:38 +0530

    dividerlines for table rows

--- src/components/ModelBuildTable.js
+++ src/components/ModelBuildTable.js
@@ -163,6 +163,7 @@ export const ModelBuildTable = ({ rows }) => {
                   </td>
                 )
               )}
+              <hr />
             </tr>
           ))}
         </tbody>
